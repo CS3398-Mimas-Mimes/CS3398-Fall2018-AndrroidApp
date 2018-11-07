747ea83b952ba689e002fa4eb4a51379adfbd7f1
zacharyking2016 <42628355+zacharyking2016@users.noreply.github.com>
2018-11-06 19:11:29 -0600

    Add files via upload

---
+++ app/src/main/java/com/example/danhpham/group2/Menu.java
@@ -0,0 +1,21 @@
+package com.example.zakking.softwareengproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+    }
+
+}
---
+++ app/src/main/java/com/example/danhpham/group2/User.java
@@ -0,0 +1,26 @@
+package com.example.zakking.softwareengproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+    }
+
+    public void goToOrders(View view){
+        Intent startOrders = new Intent(this, userOrders.class);
+        startActivity(startOrders);
+    }
+
+}
