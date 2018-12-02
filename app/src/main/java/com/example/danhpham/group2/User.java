6bbbdc4ac5089144570101e8899d831571f6a3dd
Andrew <ash109@txstate.edu>
2018-12-02 15:26:23 -0600

    Passing intents through multiple activities

--- app/src/main/java/com/example/danhpham/group2/User.java
+++ app/src/main/java/com/example/danhpham/group2/User.java
@@ -8,18 +8,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.toolbox.StringRequest;
 
 public class User extends AppCompatActivity {
+
+    String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
+        //Toast.makeText(getApplicationContext(), String.format("ID IN USER CLASS: %", id), Toast.LENGTH_SHORT).show();
+
     }
 
     public void goToOrders(View view){
         Intent startOrders = new Intent(this, userOrders.class);
+        //Toast.makeText(getApplicationContext(), String.format("ID in goToOrders(): %s", id), Toast.LENGTH_LONG).show();
+        startOrders.putExtra("id", id);
+        //Toast.makeText(getApplicationContext(), String.format("Passing ID to Order Display: %s", id), Toast.LENGTH_LONG).show();
         startActivity(startOrders);
     }
 
